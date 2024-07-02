904b0edb21aa2f625b20a07d7ea8edecf2ad57c4
kvisalpara8 <krishvisalpara2004@gmail.com>
2024-07-02 19:25:54 +0530

    Add files via upload

---
+++ 5_optimal_skills.sql
@@ -0,0 +1,46 @@
+/*
+Question: What are the most optimal skillsto learn (high-demand and high-paying skills)?
+- Identify the skills in high demand and associated with the highest average salaries for Data Analyst roles.
+- Why? Target skills that offer job security and financial benefits, offering strategic insights for career development in data analysis.
+*/
+
+WITH skills_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_location = 'India'
+    GROUP BY
+        skills_dim.skill_id
+), average_salary AS (
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND(AVG(salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND job_postings_fact.salary_year_avg IS NOT NULL
+        AND job_location = 'India'
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
